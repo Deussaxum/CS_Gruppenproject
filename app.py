d430b592c929a40f2af2b2507493cd6f988c7131
Deussaxum <147807491+Deussaxum@users.noreply.github.com>
2023-11-29 14:16:22 +0100

    interface

--- app.py
+++ app.py
@@ -1,41 +1,75 @@
 import streamlit as st
 import requests
 
-# Streamlit UI for collecting user inputs
-st.write("# CV Generator")
+# Streamlit-Benutzeroberfläche
+st.title("CV-Generator")
 
-name = st.text_input("Name", "Julian Gottstein")
-address = st.text_input("Address", "Rorschacher Strasse 116, 900 St. Gallen")
-mobile = st.text_input("Mobile", "+49 (157) 54058312")
-email = st.text_input("Email", "juliangottstein@gmx.com")
+# Persönliche Informationen
+st.header("Personal Information")
+name = st.text_input("Name")
+address = st.text_input("Adresse")
+phone = st.text_input("Telefonnummer")
+email = st.text_input("E-Mail")
 
-if st.button("Generate CV"):
-    # Generate LaTeX code with placeholders for user information
-    latex_code = fr"""
-    \documentclass[a4paper,8pt]{{article}}
+# Education
+st.header("Education")
+university1 = st.text_input("Universität/Schule 1")
+location_us1 = st.text_input("Standort 1")
+majorus1 = st.text_input("Studiengang 1")
+timeus1 = st.text_input("Zeitraum 1")
+courses1 = st.text_input("Kurse 1")
+gpa1 = st.text_input("GPA 1")
+clubs1 = st.text_input("Clubs/Aktivitäten 1")
 
-    % ... (rest of the LaTeX preamble remains the same)
+university2 = st.text_input("Universität/Schule 2", "")
+location_us2 = st.text_input("Standort 2", "")
+majorus2 = st.text_input("Studiengang 2", "")
+timeus2 = st.text_input("Zeitraum 2", "")
+courses2 = st.text_input("Kurse 2", "")
+gpa2 = st.text_input("GPA 2", "")
+clubs2 = st.text_input("Clubs/Aktivitäten 2", "")
 
-    \begin{{document}}
+# Professional Experience
+st.header("Professional Experience")
+experience1 = st.text_input("Erfahrung 1")
+location_e1 = st.text_input("Standort Erfahrung 1")
+position1 = st.text_input("Position 1")
+timee1 = st.text_input("Zeitraum Erfahrung 1")
+task11 = st.text_area("Aufgaben 1", height=100)
+task12 = st.text_area("Aufgaben 2", height=100)
+task13 = st.text_area("Aufgaben 3", height=100)
 
-    \begin{{tabularx}}{{\linewidth}}{{@{{}} C @{{}}}}
-    \color[HTML]{{1C033C}} \Huge{{\textbf{{{name}}}}} \\[6pt]
-    \\
-    \textcolor[HTML]{{1C033C}}{{Address: {address}}} \\
-    \textcolor[HTML]{{1C033C}}{{Mobile: {mobile} $|$}}
-    \textcolor[HTML]{{1C033C}}{{Email: {email}}}
-    \end{{tabularx}}
+experience2 = st.text_input("Erfahrung 2", "")
+location_e2 = st.text_input("Standort Erfahrung 2", "")
+position2 = st.text_input("Position 2", "")
+timee2 = st.text_input("Zeitraum Erfahrung 2", "")
+task21 = st.text_area("Aufgaben 1", height=100)
+task22 = st.text_area("Aufgaben 2", height=100)
+task23 = st.text_area("Aufgaben 3", height=100)
 
-    \end{{document}}
-    """
+experience3 = st.text_input("Erfahrung 3", "")
+location_e3 = st.text_input("Standort Erfahrung 3", "")
+position3 = st.text_input("Position 3", "")
+timee3 = st.text_input("Zeitraum Erfahrung 3", "")
+task31 = st.text_area("Aufgaben 1", height=100)
+task32 = st.text_area("Aufgaben 2", height=100)
+task33 = st.text_area("Aufgaben 3", height=100)
 
-    # Make an HTTP POST request to Overleaf's API
-    response = requests.post("https://www.overleaf.com/docs", data={"snip_uri": latex_code})
+# Extracurriculat Activities / Engagement
+st.header("Extracurriculat Activities / Engagement")
+extracurricular1 = st.text_input("Extrakurrikulare Aktivitäten")
+additionaleducation1 = st.text_input("Zusätzliche Bildung")
+certificates1 = st.text_input("Zertifikate und Errungenschaften")
 
-    if response.status_code == 200:
-        st.success("CV generated successfully on Overleaf! You can access it there.")
-        st.write("You can download the generated PDF from the following link:")
-        overleaf_url = response.url  # Use the URL directly
-        st.write(f"[Download CV PDF from Overleaf]({overleaf_url})")
-    else:
-        st.error("Error generating CV. Please try again later.")
+# Skills & Interest
+st.header("Skills & Interest")
+languages1 = st.text_input("Sprachen")
+computer1 = st.text_input("Computerkenntnisse")
+interests1 = st.text_input("Interessen")
+
+# Button zum Erstellen des CVs
+if st.button("CV Erstellen"):
+    # Hier die Logik zum Einsetzen der Daten in die LaTeX-Vorlage und deren Verarbeitung hinzufügen
+    # ...
+
+    st.success("CV erfolgreich erstellt!")
