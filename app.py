15c0ac74ae7fac26e5b436f828e797a7e483a262
Deussaxum <147807491+Deussaxum@users.noreply.github.com>
2023-11-29 11:07:23 +0100

    s

--- app.py
+++ app.py
@@ -1,77 +1,22 @@
 import streamlit as st
 import requests
 
-latex_preamble = r"""
-\documentclass[a4paper,8pt]{article}
+# Streamlit UI for collecting user inputs
+st.write("# CV Generator")
 
-\usepackage{parskip}
-\usepackage{hologo}
-\usepackage{fontspec}
-% ... (rest of the preamble remains the same)
-"""
+name = st.text_input("Name", "Julian Gottstein")
+address = st.text_input("Address", "Rorschacher Strasse 116, 900 St. Gallen")
+mobile = st.text_input("Mobile", "+49 (157) 54058312")
+email = st.text_input("Email", "juliangottstein@gmx.com")
 
-def compile_pdf(name, address, mobile, email):
+if st.button("Generate CV"):
+    # Generate LaTeX code with placeholders for user information
     latex_code = fr"""
     \documentclass[a4paper,8pt]{{article}}
 
-    \usepackage{{parskip}}
-    \usepackage{{hologo}}
-    \usepackage{{fontspec}}
-
-    %other packages for formatting
-    \RequirePackage{{color}}
-    \RequirePackage{{graphicx}}
-    \usepackage[usenames,dvipsnames]{{xcolor}}
-    \usepackage[scale=0.9, top=.4in, bottom=.4in]{{geometry}}
-    \usepackage{{enumitem}}
-
-    %tabularx environment
-    \usepackage{{tabularx}}
-
-    %for lists within experience section
-    \usepackage{{enumitem}}
-
-    % centered version of 'X' col. type
-    \newcolumntype{{C}}{{>{{\centering\arraybackslash}}X}}
-
-    %to prevent spillover of tabular into next pages
-    \usepackage{{supertabular}}
-    \usepackage{{tabularx}}
-    \newlength{{\fullcollw}}
-    \setlength{{\fullcollw}}{{0.42\textwidth}}
-
-    %custom \section
-    \usepackage{{titlesec}}
-    \usepackage{{multicol}}
-    \usepackage{{multirow}}
-
-    %CV Sections inspired by:
-    %http://stefano.italians.nl/archives/26
-    \titleformat{{\section}}{{\Large\scshape\raggedright}}{{}}{{0em}}{{}}[\titlerule]
-    \titlespacing{{\section}}{{1pt}}{{2pt}}{{2pt}}
-
-    %for publications
-    \usepackage[style=authoryear,sorting=ynt, maxbibnames=2]{{biblatex}}
-
-    %Setup hyperref package, and colours for links
-    \usepackage[unicode, draft=false]{{hyperref}}
-    \color[HTML]{{110223}}%{{1C033C}}
-    \addbibresource{{citations.bib}}
-    \setlength{{\bibitemsep}}{{1em}}
-
-    %for social icons
-    \usepackage{{fontawesome5}}
-    % \usepackage{{times}}
-
-    % For underline
-    \usepackage[normalem]{{ulem}}
-
-    \setmainfont{{Arial}}  % Set it to whatever you like
+    % ... (rest of the LaTeX preamble remains the same)
 
     \begin{{document}}
-
-    % non-numbered pages
-    \pagestyle{{empty}}
 
     \begin{{tabularx}}{{\linewidth}}{{@{{}} C @{{}}}}
     \color[HTML]{{1C033C}} \Huge{{\textbf{{{name}}}}} \\[6pt]
@@ -80,38 +25,17 @@ def compile_pdf(name, address, mobile, email):
     \textcolor[HTML]{{1C033C}}{{Mobile: {mobile} $|$}}
     \textcolor[HTML]{{1C033C}}{{Email: {email}}}
     \end{{tabularx}}
+
     \end{{document}}
     """
 
-    # Replace placeholders in LaTeX code with user inputs
-    latex_code = latex_code.replace("<<NAME>>", name)
-    latex_code = latex_code.replace("<<ADDRESS>>", address)
-    latex_code = latex_code.replace("<<MOBILE>>", mobile)
-    latex_code = latex_code.replace("<<EMAIL>>", email)
-
-    data = {
-        "compiler": "pdflatex",
-        "code": latex_code,
-    }
-
-    response = requests.post("https://latexonline.cc/compile", data=data)
+    # Make an HTTP POST request to Overleaf's API
+    response = requests.post("https://www.overleaf.com/docs", data={"snip_uri": latex_code})
 
     if response.status_code == 200:
-        pdf_data = response.content
-        return pdf_data
+        st.success("CV generated successfully on Overleaf! You can access it there.")
+        st.write("You can download the generated PDF from the following link:")
+        overleaf_url = response.json().get("url")
+        st.write(f"[Download CV PDF from Overleaf]({overleaf_url})")
     else:
-        st.error("Error compiling PDF")
-
-st.write("# Applify")
-st.latex(r"\LaTeX")
-
-st.write("Enter your information here:")
-name = st.text_input("Name", "Julian Gottstein")
-address = st.text_input("Address", "Rorschacher Strasse 116, 900 St. Gallen")
-mobile = st.text_input("Mobile", "+49 (157) 54058312")
-email = st.text_input("Email", "juliangottstein@gmx.com")
-
-if st.button("Compile PDF"):
-    pdf_data = compile_pdf(name, address, mobile, email)
-    if pdf_data is not None:
-        st.download_button("Download PDF", pdf_data, file_name='CV.pdf', mime='application/pdf')
+        st.error("Error generating CV. Please try again later.")
