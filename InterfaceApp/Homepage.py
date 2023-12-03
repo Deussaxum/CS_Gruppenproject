8e41d492e47bd3da5d87b496bfb1555053148f98
MaxQuast <147815120+MaxQuast@users.noreply.github.com>
2023-12-03 17:11:46 +0100

    Add files via upload

--- InterfaceApp/Homepage.py
+++ InterfaceApp/Homepage.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
-
 st.set_page_config(
-    page_title="Career Compass",
+    page_title="Career Compass 🧭",
 )
 
-st.title("CV Generator")
+st.title("CV Generator 📃")
+st.header("Creating your own CV has never been easier!")
+st.subheader("👈🏼 Make use of our pre-defined templates")
+st.write("HOW IT WORKS")
+st.write("1. Determine which industry suits you best by taking our test based on the Big 5 Personality Traits.")
+st.write("2. Choose the industry you want your CV to be tailored to.")
+st.write("3. Easily import your data from LinkedIn to streamline your CV creation process!")
+
 st.sidebar.success("Please select a page Above.")
-
-
