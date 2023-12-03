dd9c466fb2179be558e1dd2b63313408b386518b
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-03 21:49:26 +0100

    s

--- API.py
+++ API.py
@@ -1,27 +1,70 @@
-<<<<<<< HEAD
-print("Hello World")
-=======
-#LinkedIn API
-
-
-#Installation of required packages
-pip install streamlit
-pip install requests
-pip install requests_oauthlib
-pip install oauthlib
-
-#Import of required packages
 import streamlit as st
-import requests
 from requests_oauthlib import OAuth2Session
-from urllib.parse import urlparse, parse_qs
+from requests.exceptions import HTTPError
 
 # Constants
-CLIENT_ID = 'YOUR_CLIENT_ID'
-CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
-REDIRECT_URI = 'http://localhost:8501'  # This should match your Streamlit app's address
+CLIENT_ID = 'Key_01_here'
+CLIENT_SECRET = 'Key_02_here'
+REDIRECT_URI = 'Your_Redirect_URI_here'  # Your Streamlit app's address
 AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
 TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = ['r_liteprofile']  # Define your scopes here
+SCOPE = ['openid', 'profile', 'email']  # Scopes for OpenID Connect
 
->>>>>>> b641975d5625cdc7fde4b52657e876e5ca07777d
+# Function to parse query string
+def get_query_params():
+    return st.experimental_get_query_params()
+
+# Start the OAuth process
+def start_oauth():
+    linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+    authorization_url, state = linkedin.authorization_url(AUTHORIZATION_BASE_URL)
+    # Save the state in session for later use
+    st.session_state['oauth_state'] = state
+    # Redirect to LinkedIn for authorization
+    st.markdown(f"[Log in with LinkedIn]({authorization_url})", unsafe_allow_html=True)
+
+# Fetch token and user info
+def fetch_token_and_user_info(code):
+    try:
+        linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+        # Add the client_secret parameter to the fetch_token() method call
+        token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
+        # Save the token in session
+        st.session_state['oauth_token'] = token
+
+        # Fetch user info
+        user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
+        st.session_state['user_info'] = user_info
+
+        # Fetch user email
+        email_info = linkedin.get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))').json()
+        st.session_state['email_info'] = email_info.get('elements', [])[0].get('handle~', {}).get('emailAddress', '')
+
+    except HTTPError as e:
+        st.error(f'An HTTP error occurred: {e.response.status_code}')
+    except Exception as e:
+        st.error(f'An error occurred: {e}')
+
+# Main App
+def main():
+    st.title("LinkedIn OpenID Connect Authentication")
+
+    query_params = get_query_params()
+    code = query_params.get("code")
+
+    if "oauth_token" not in st.session_state:
+        if not code:
+            start_oauth()
+        else:
+            fetch_token_and_user_info(code[0])
+            st.success("Authentication successful!")
+
+    if "user_info" in st.session_state:
+        st.write("Your LinkedIn profile information:")
+        st.json(st.session_state['user_info'])
+
+        st.write("Your LinkedIn email information:")
+        st.write(st.session_state['email_info'])
+
+if __name__ == "__main__":
+    main()
