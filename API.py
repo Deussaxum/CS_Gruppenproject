55007412c9e9f5fe5d42888e1c1d19bcb5e895e0
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 17:54:39 +0100

    Update API - Country selection & design

--- API.py
+++ API.py
@@ -1,29 +1,23 @@
 import streamlit as st
 import requests
 
-# Define the API key and headers
-api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'  # Replace with your actual API key
-headers = {'Authorization': 'Bearer ' + api_key}
-api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
-
 # URL of the LinkedIn logo
 logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
 
-# Set the geo_id parameter which the user cannot change
-geo_id = '101282230'
+# Define the API key and headers
+api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'
+headers = {'Authorization': 'Bearer ' + api_key}
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 
-# Function to capitalize labels
-def capitalize_labels(options):
-    return [option.replace('_', ' ').title() for option in options]
-
-# Function to display jobs
-def display_jobs(jobs):
-    for job in jobs:
-        st.write(f"**{job['job_title']}** at **{job['company']}**")
-        st.write(f"Location: {job['location']}")
-        st.write(f"Listed on: {job['list_date']}")
-        st.write(f"[Job Details]({job['job_url']})")
-        st.write("---------")
+# Country to geo_id mapping
+country_geo_id_mapping = {
+    'Austria': '103883259',
+    'France': '105015875',
+    'Germany': '101282230',
+    'Italy': '103350119',
+    'Switzerland': '106693272',
+    'USA': '103644278'
+}
 
 # Initialize session state variables
 if 'jobs' not in st.session_state:
@@ -31,46 +25,56 @@ if 'jobs' not in st.session_state:
     st.session_state['next_page_url'] = None
     st.session_state['search_initiated'] = False
 
-# Adjust the column widths to align the logo with the end of the input fields
-col1, col2 = st.columns([0.9, 0.1])
-
-# Display the title in the first column
-with col1:
-    st.markdown("<h1 style='text-align: left;'>Job Search</h1>", unsafe_allow_html=True)
-
-# Display the logo in the second column
-with col2:
-    st.image(logo_url, width=60)  # Reduce the size of the picture by 60%
+# Title and logo layout
+header_col, logo_col = st.columns([0.85, 0.15])
+with header_col:
+    st.header("Job Search")
+with logo_col:
+    st.image(logo_url, width=60)  # Adjust width as needed
 
 # Create search fields for user input
-job_type = st.selectbox('Job Type', capitalize_labels(['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer']))
-experience_level = st.selectbox('Experience Level', capitalize_labels(['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director']))
-when = st.selectbox('When', capitalize_labels(['anytime', 'yesterday', 'past-week', 'past-month']))
-flexibility = st.selectbox('Flexibility', capitalize_labels(['anything', 'remote', 'on-site', 'hybrid']))
-keyword = st.text_input('Keyword', '')
-
-# Container to display jobs below the input fields
-jobs_container = st.container()
+country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
+job_type = st.selectbox('Employment type', ['Anything', 'Full Time', 'Part Time', 'Internship', 'Contract', 'Temporary', 'Volunteer'])
+experience_level = st.selectbox('Experience level', ['Anything', 'Internship', 'Entry Level', 'Associate', 'Mid-Senior Level', 'Director'])
+when = st.selectbox('Job posted on', ['Anytime', 'Yesterday', 'Past-Week', 'Past-Month'])
+flexibility = st.selectbox('Flexibility', ['Anything', 'Remote', 'On-Site', 'Hybrid'])
+keyword = st.text_input('Keywords', '')
 
 # Button to perform the API call
 if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True
     st.session_state['jobs'] = []  # Clear previous jobs
+    selected_geo_id = country_geo_id_mapping[country]  # Use the selected country's geo_id
     params = {
-        'job_type': job_type.lower().replace(' ', '_'),
-        'experience_level': experience_level.lower().replace(' ', '_'),
-        'when': when.lower().replace(' ', '_'),
-        'flexibility': flexibility.lower().replace(' ', '_'),
-        'geo_id': geo_id,
+        'job_type': job_type.replace(' ', '_').lower(),
+        'experience_level': experience_level.replace(' ', '_').lower(),
+        'when': when.replace(' ', '_').lower(),
+        'flexibility': flexibility.replace(' ', '_').lower(),
+        'geo_id': selected_geo_id,
         'keyword': keyword
     }
     response = requests.get(api_endpoint, params=params, headers=headers)
     if response.status_code == 200:
         st.session_state['jobs'] = response.json().get('job', [])
         st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-        display_jobs(st.session_state['jobs'])
     else:
         st.error(f"Failed to retrieve jobs: {response.status_code}")
+
+# Container to display jobs below the button
+jobs_container = st.container()
+
+# Function to display jobs
+def display_jobs(jobs, container):
+    for job in jobs:
+        container.write(f"**{job['job_title']}** at **{job['company']}**")
+        container.write(f"Location: {job['location']}")
+        container.write(f"Listed on: {job['list_date']}")
+        container.write(f"[Job Details]({job['job_url']})")
+        container.write("---------")
+
+# Display jobs if any
+if st.session_state['jobs']:
+    display_jobs(st.session_state['jobs'], jobs_container)
 
 # Function to load more jobs
 def load_more_jobs():
@@ -79,9 +83,9 @@ def load_more_jobs():
         response = requests.get(next_page_url, headers=headers)
         if response.status_code == 200:
             new_jobs = response.json().get('job', [])
-            st.session_state['jobs'].extend(new_jobs)  # Append new jobs
+            st.session_state['jobs'].extend(new_jobs)
             st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-            display_jobs(st.session_state['jobs'])
+            display_jobs(st.session_state['jobs'], jobs_container)
         else:
             st.error(f"Failed to load more jobs: {response.status_code}")
 
