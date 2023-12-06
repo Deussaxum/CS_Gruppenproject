53512c022af065e19af99ce4a6a695aebdc6c8ef
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-06 03:11:28 +0100

    Update APIPeople.py

--- APIPeople.py
+++ APIPeople.py
@@ -1,11 +1,25 @@
 import streamlit as st
 import requests
-import json
 
 # Define your API key and headers
 api_key = '_EIqMpWEbOnJLoQvNFz1CQ'  # Be sure to replace with your actual API key
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+
+# Streamlit app layout
+st.set_page_config(layout="wide")  # Set the page layout to wide
+
+# Create columns for the header and the LinkedIn logo
+header_col, logo_col = st.columns([0.9, 0.1])
+
+# Use the first column to display the app title
+with header_col:
+    st.title('LinkedIn Profile Filler')
+
+# Use the second column to display the LinkedIn logo
+with logo_col:
+    linkedin_logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+    st.image(linkedin_logo_url, width=50)
 
 # Function to extract information from API response
 def extract_info(jsondata):
@@ -16,38 +30,33 @@ def extract_info(jsondata):
     }
     return extracted_info
 
-# Streamlit app layout
-st.title('LinkedIn Profile Filler')
-
-# Input field for LinkedIn profile URL
-linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', 'https://www.linkedin.com/in/...')
-
-# Display the clickable image
-if st.button('Retrieve Information'):
-    retrieve_info()
-
+# Function to retrieve information
 def retrieve_info():
     params = {'linkedin_profile_url': linkedin_profile_url}
-    
-    # Make a request to the API
     response = requests.get(api_endpoint, params=params, headers=headers)
-    
     if response.status_code == 200:
         data = response.json()
         info = extract_info(data)
-        
-        # Display the extracted information
-        st.subheader('Profile Information:')
-        st.write('Full Name:', info['full_name'])
-        st.write('City:', info['city'])
-        
-        st.subheader('Experiences:')
-        for exp in info['experiences']:
-            st.write('Title:', exp.get('title', 'Not available'))
-            st.write('Company:', exp.get('company', 'Not available'))
-            st.write('Description:', exp.get('description', 'Not available'))
-            st.write('---')  # Separator line
+        display_info(info)
     else:
         st.error(f"Failed to retrieve profile information: HTTP {response.status_code}")
 
-# Load the image from the
+# Function to display profile information
+def display_info(info):
+    st.subheader('Profile Information:')
+    st.write('Full Name:', info['full_name'])
+    st.write('City:', info['city'])
+    
+    st.subheader('Experiences:')
+    for exp in info['experiences']:
+        st.write('Title:', exp.get('title', 'Not available'))
+        st.write('Company:', exp.get('company', 'Not available'))
+        st.write('Description:', exp.get('description', 'Not available'))
+        st.write('---')  # Separator line
+
+# Input field for LinkedIn profile URL
+linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL')
+
+# Button to trigger the information retrieval
+if st.button('Retrieve Information'):
+    retrieve_info()
